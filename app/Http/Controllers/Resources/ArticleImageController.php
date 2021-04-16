473ad750d5500268c6d9c1e610f4451ffb3699c7
1 <1>
2021-04-16 18:04:11 +0800

    111

---
+++ app/Http/Controllers/Resources/ArticleImageController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Resources;
+
+use App\Http\Requests\ArticleCover\Store;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Date;
+
+class ArticleImageController extends Controller
+{
+    public function store(Store $request): JsonResponse
+    {
+        $result = [
+            'success' => 1,
+            'message' => 'success',
+            'url'     => '',
+        ];
+
+        foreach (config('bjyblog.upload_disks') as $disk) {
+            $result['url'] = '/' . $request->file('image')->store('uploads/article/' . Date::now()->format('Ymd'), $disk);
+        }
+
+        return response()->json($result);
+    }
+}
---
+++ app/Observers/SiteObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Observers;
+
+use App\Notifications\SiteAudit;
+
+class SiteObserver extends BaseObserver
+{
+    /**
+     * @param \App\Models\Site $site
+     *
+     * @return void
+     */
+    public function updated($site)
+    {
+        // restore() triggering both restored() and updated()
+        if(! $site->isDirty('deleted_at')){
+            // $site->audit is string
+            if ($site->isDirty('audit') && intval($site->audit) === 1) {
+                $site->socialiteUser->notify(new SiteAudit());
+            }
+
+            flash_success('修改成功');
+        }
+    }
+}
